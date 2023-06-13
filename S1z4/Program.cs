6dd23910cbfd269f36ce04ee9350affd683b82dd
Mkliv_NB <mchlkiselev@gmail.com>
2023-06-13 19:57:32 +0300

    Код для задачи 4

--- S1z4/Program.cs
+++ S1z4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число A:");
+int NumA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int NumB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C:");
+int NumC = Convert.ToInt32(Console.ReadLine());
+int max = NumA;
+if (NumB > max)
+{
+    max = NumB;
+}
+if (NumC > max)
+{
+    max = NumC;
+}
+
+Console.WriteLine("Максимальное число {0} ", max);
