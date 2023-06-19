bd2b018e85d96155639e336228ba28c1569d0243
Mkliv_NB <mchlkiselev@gmail.com>
2023-06-19 10:48:14 +0300

    Добавлена задача 10

---
+++ S2z10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число:");
+string input = Console.ReadLine();
+
+if (input.Length == 3 && int.TryParse(input, out int number))
+{
+    int secDigit = (number / 10) % 10;
+    Console.WriteLine("Вторая цифра числа: {0}", secDigit);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод. Введите трёхзначное число.");
+}
