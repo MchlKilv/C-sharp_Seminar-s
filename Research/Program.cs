358d936249d67bd9453487000d84098d9862507c
Mkliv_NB <mchlkiselev@gmail.com>
2023-06-19 10:46:31 +0300

    Research work

--- Research/Program.cs
+++ Research/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int thirdLeftDigit = -1;
+int count = 0;
+// Получаем абсолютное значение числа
+number = Math.Abs(number);
+
+// Проверяем, достаточно ли цифр в числе для нахождения третьей слева цифры
+if (number > 99)
+{
+    while (count < number)
+    {
+        if (number > 100)
+        {
+            thirdLeftDigit = number % 10;
+        }
+        number /= 10;
+        count++;
+    }
+}
+
+if (thirdLeftDigit != -1)
+{
+    Console.WriteLine($"Третья слева цифра: {thirdLeftDigit}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
