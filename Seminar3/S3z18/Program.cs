c2334d08f4b1e536cfbdede958074841c93508c6
[MchlK] <[mchlkiselev@gmail.com]>
2023-06-27 13:41:14 +0300

    s3z18

---
+++ Seminar3/S3z18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер коорд четверти 1-4");
+int quart = Convert.ToInt32(Console.ReadLine());
+
+if (quart == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if (quart == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (quart == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (quart == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
+else
+{
+    Console.WriteLine("нет такой четверти");
+}
