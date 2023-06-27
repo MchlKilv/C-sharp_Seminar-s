ea5a3ee1d904390a5c48180d73f9a50c59b668a1
[MchlK] <[mchlkiselev@gmail.com]>
2023-06-27 18:29:15 +0300

    s4z29

---
+++ Seminar4/S4z29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+Console.WriteLine("Введите 8 элементов массива:");
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Элемент {i + 1}: ");
+    string input = Console.ReadLine();
+    int element;
+
+    if (int.TryParse(input, out element))
+    {
+        array[i] = element;
+    }
+    else
+    {
+        Console.WriteLine("Неверный формат числа. Попробуйте снова.");
+        i--; // Уменьшаем счетчик, чтобы повторно запросить ввод для текущего элемента
+    }
+}
+Console.WriteLine("Элементы массива:");
+foreach (int element in array)
+{
+    Console.WriteLine(element);
+}
