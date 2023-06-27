bb91ca4f70c24e124e052c65ae1e93fa0c376466
[MchlK] <[mchlkiselev@gmail.com]>
2023-06-27 18:09:48 +0300

    s4z27

---
+++ Seminar4/S4z27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numOut = number;
+int digitSum = 0;
+while (Math.Abs(number) > 0)
+{
+   digitSum += Math.Abs(number) % 10;
+   number /= 10;
+}
+Console.WriteLine($"Сумма цифр числа {numOut} равна {digitSum}");
