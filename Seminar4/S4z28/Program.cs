cac4ed7f9fa5fc6eacc1bd4b3a04cb63926ab4d6
[MchlK] <[mchlkiselev@gmail.com]>
2023-06-27 19:14:38 +0300

    s4z28

---
+++ Seminar4/S4z28/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int ReadInt()
+{
+    Console.WriteLine("Введите целое число:");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int Mult(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+int number = ReadInt();
+int mult = Mult(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} равно {mult}");
