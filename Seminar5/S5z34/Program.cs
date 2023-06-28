e02363a7921960978b3e6d7631f9e11f314b0192
[MchlK] <[mchlkiselev@gmail.com]>
2023-06-28 23:22:00 +0300

    S5z34

---
+++ Seminar5/S5z34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = GetRandomArray();
+
+int[] GetRandomArray()
+{
+    int[] randomArray = new int[size];
+
+    for (int i = 0; i < randomArray.Length; i++)
+    {
+        randomArray[i] = Random.Shared.Next(100, 1000);
+    }
+
+    return randomArray;
+}
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"{count} четных чисел в массиве: [{string.Join(", ", array)}]");
