57773b456dbec5aad9511eaa26936dc4b4ac54f3
[MchlK] <[mchlkiselev@gmail.com]>
2023-06-28 21:37:25 +0300

    S5z33

---
+++ Seminar5/S5z33/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве.
+
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> нет
+
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] GetRandomArray()
+{
+    int[] arrayRandom = new int[size];
+
+    for (int i = 0; i < arrayRandom.Length; i++)
+    {
+        arrayRandom[i] = Random.Shared.Next(-9, 10);
+    }
+
+    return arrayRandom;
+}
+
+Console.Write("Введите число для поиска в массиве: ");
+int searchNumber = Convert.ToInt32(Console.ReadLine());
+int[] array = GetRandomArray();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (searchNumber == array[i])
+    {
+        count++;
+        break;
+    }
+}
+if (count == 1)
+{
+    Console.WriteLine($"Да, число {searchNumber} в массиве [{string.Join(", ", array)}]");
+}
+else
+{
+    Console.WriteLine($"Нет, число {searchNumber} не в массиве [{string.Join(", ", array)}]");
+}
