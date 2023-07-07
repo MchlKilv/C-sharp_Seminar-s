c1cef439895b0d875121257b46f7903915cc519a
[MchlK] <[mchlkiselev@gmail.com]>
2023-07-07 23:07:06 +0300

    S9z65

---
+++ Seminar9/S9z65/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+int numM = 1;
+int numN = 5;
+
+void NaturalsNumbers(int endNum, int startNum)
+{
+    if (endNum <= startNum - 1) return;
+    NaturalsNumbers(endNum - 1, startNum);
+    //Console.Write($" {endNumber}");
+    Console.Write(endNum + " ");
+}
+NaturalsNumbers(numN, numM);
